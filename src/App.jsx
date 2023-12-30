753bf74de0eef68845d7aa1a034362aebad01a4e
Tolunaykns <119815271+Tolunaykns@users.noreply.github.com>
2023-12-30 21:53:04 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Sidenav from './components/Sidenav'
+
+function App() {
+
+  return (
+    <>
+      <Sidenav 
+      
+      />
+    </>
+  )
+}
+
+export default App
